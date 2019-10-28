0528d4d099a86fddac587a902fb1dd82dd93c38e
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro-2.local>
2019-10-28 16:44:40 -0400

    url post route

--- express_server.js
+++ express_server.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = function(length) {
+
+  let output = '';
+  for (let i = 0; i < length; i++) {
+    let char = Math.round(Math.random() * 62);
+    if (char < 10) {
+      char += 48;
+    } else if (char < 36) {
+      char += 55;
+    } else {
+      char += 61;
+    }
+    output += String.fromCharCode(char);
+  }
+  return output;
 };
 
 app.set("view engine", "ejs");
@@ -30,9 +49,11 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-// app.post("/urls", (req, res) => {
-//   res.render("urls_new");
-// });
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");
+  // urlDatabase[generateRandomString(6)] = req.body.longURL;
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");

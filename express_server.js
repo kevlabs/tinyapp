2a81a99e6adc20a5a72d735aa4edb7fa1c76aedc
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro-2.local>
2019-10-28 16:21:21 -0400

    Added new url form

--- express_server.js
+++ express_server.js
@@ -17,6 +17,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL]) res.render('urls_show', { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] });
 });
@@ -25,6 +29,10 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+
+// app.post("/urls", (req, res) => {
+//   res.render("urls_new");
+// });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
